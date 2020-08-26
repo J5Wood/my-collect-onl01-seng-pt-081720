f03563919f11f5b6dc0fd6677416a51906eaff1c
Jeremy Wood <js.wood206@gmail.com>
2020-08-26 03:17:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,11 @@
 def my_collect(array)
   return "No block given!" unless block_given?
+  counter = 0
+  return_list = []
+  while counter < array.length
+    yield array[counter]
+    return_list <<
+    counter +=1
+  end
+  return_list
 end
